eda093cba3ec738e95e09727286e934532b0400a
RudenkoOleksandr02 <gitemail02@gmail.com>
2026-06-30 18:14:04 +0300

    added new api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://directus-production-539a.up.railway.app';
+const url = 'https://directus-production-2d7e.up.railway.app';
 const instance = axios.create({
     baseURL: `${url}/graphql`,
     headers: {'content-type': 'application/json'}
